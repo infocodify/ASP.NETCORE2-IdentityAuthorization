770171b51ac76b3a410cba8c15f732e9c10671d9
infocodify <bledinjo@hotmail.com>
2018-08-23 13:49:30 -0400

    To create Identity Autentication and Authorization in asp.Net Core 2 follow the steps:

---
+++ IdentityAuth/IdentityAuth/Pages/Tutorials.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityAuth.Pages
+{
+
+    //Authorization Required to access Tutorials
+    [Authorize]
+    public class TutorialsModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Your application description page.";
+        }
+    }
+}
